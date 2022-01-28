133e989c02b1aad113f039e40a3c95b37ddb731d
MaghnusM <m.h.mareneck@gmail.com>
2022-01-27 20:48:42 -0500

    new changes

---
+++ build/precache-manifest.cfb2b1262e480147921b15da20ec5d46.js
@@ -0,0 +1,98 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "10cd702481045e18762b06416c348253",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9d79e084263c408cf92a",
+    "url": "/static/css/main.3803c59b.chunk.css"
+  },
+  {
+    "revision": "334fad2ef3481ca918a6",
+    "url": "/static/js/2.31fc8ffa.chunk.js"
+  },
+  {
+    "revision": "9d79e084263c408cf92a",
+    "url": "/static/js/main.91f739d7.chunk.js"
+  },
+  {
+    "revision": "f67c199efb9f2c29f389",
+    "url": "/static/js/runtime-main.f294effb.js"
+  },
+  {
+    "revision": "80f10a4226e8c36c28eb14a0c9ed581a",
+    "url": "/static/media/artbook1.80f10a42.png"
+  },
+  {
+    "revision": "64d0f6eb1f84ad654e36eb972f978a1b",
+    "url": "/static/media/artbook2.64d0f6eb.png"
+  },
+  {
+    "revision": "78ac9591d7735695cc1b07e1b501ecd4",
+    "url": "/static/media/ddlogo.78ac9591.png"
+  },
+  {
+    "revision": "08aaa6264efc339d145b1316c5199eeb",
+    "url": "/static/media/dino1.08aaa626.png"
+  },
+  {
+    "revision": "bab5e6f4852bd66cc54c1414053c8f84",
+    "url": "/static/media/dino2.bab5e6f4.png"
+  },
+  {
+    "revision": "100db557eb57cac111f0fc9d0a1654a2",
+    "url": "/static/media/dino3.100db557.png"
+  },
+  {
+    "revision": "e0dc6a8a427f0c4971a4a85abfeaf658",
+    "url": "/static/media/dinogif1.e0dc6a8a.gif"
+  },
+  {
+    "revision": "2e6447b55025644292592424be2edafb",
+    "url": "/static/media/dinotopiaisreal.2e6447b5.png"
+  },
+  {
+    "revision": "cdda202b89ccc9c88633c7d03a28aab0",
+    "url": "/static/media/discord.cdda202b.png"
+  },
+  {
+    "revision": "3437c10597c1526c3dbd98c737c2bcae",
+    "url": "/static/media/discordlogo.3437c105.svg"
+  },
+  {
+    "revision": "d6982ae81d729be0e44e17b5dea30cc0",
+    "url": "/static/media/sample1.d6982ae8.png"
+  },
+  {
+    "revision": "2ae09a7409672810ed2ea8a09ea6d5df",
+    "url": "/static/media/sample2.2ae09a74.png"
+  },
+  {
+    "revision": "3a4182a03c86e4016e61639fe183ed1e",
+    "url": "/static/media/sample3.3a4182a0.png"
+  },
+  {
+    "revision": "6edf67984916744d954e721bcf774c67",
+    "url": "/static/media/sample4.6edf6798.png"
+  },
+  {
+    "revision": "eda05f1ce2bdcf2855f6892027d72d14",
+    "url": "/static/media/sample5.eda05f1c.png"
+  },
+  {
+    "revision": "39a35912e0c8335d1e2c54c6326004f1",
+    "url": "/static/media/sample6.39a35912.png"
+  },
+  {
+    "revision": "76b8845dd2668661234eb26985b214bc",
+    "url": "/static/media/sample7.76b8845d.png"
+  },
+  {
+    "revision": "01ce92751eb413bcc083fb80ee57e66d",
+    "url": "/static/media/sample8.01ce9275.png"
+  },
+  {
+    "revision": "8cea1391b8e0717ea6532ebfa5cf4a34",
+    "url": "/static/media/twitter-logo.8cea1391.svg"
+  }
+]);
